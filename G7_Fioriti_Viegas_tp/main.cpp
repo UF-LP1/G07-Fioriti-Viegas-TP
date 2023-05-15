66020fbc9007e1aef8bcdb5dc2582a584b23fc8d
Fedefioriti <ffioriti10@gmail.com>
2023-05-15 19:13:37 -0300

    cambios en el main

--- G7_Fioriti_Viegas_tp/main.cpp
+++ G7_Fioriti_Viegas_tp/main.cpp
@@ -3,14 +3,27 @@
 #include <string>
 #include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/cotillon.h"
 #include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/cliente.h"
+#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/disfraces.h"
+#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/globos.h"
+#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/golosinas.h"
+#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/manteles.h"
+#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/molde.h"
+#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/reposteria.h"
+#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/vajilla_descartable.h"
+#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/vela.h"
 
 using namespace std;
+
 int main() {
 
     cotillon producto(10.0, 20, "disfrazate", mascara);
     cotillon producto1(14.5, 15, "papelito", papel_picado);
     cotillon producto2(5.0, 30, "laLuz", fluorescentes);
     cotillon producto3(2.3, 13, "espumin", espuma);
+    disfraces producto4(30.0, 3, "disfrazate");
+    globos producto5("amarillo", 20, 1.0, "chico", "bombucha");
+    golosinas producto6("fellfort", 1.0, 50);
+    manteles producto7(precio, stock, color, tamanio, marca);
     forward_list<articulos> lista;
     lista.push_front(producto);
     lista.push_front(producto1);
