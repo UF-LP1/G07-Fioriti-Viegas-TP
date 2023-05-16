2a513dd49032a8af06bed8e3a21b8007562c6d10
Fedefioriti <113440371+Fedefioriti@users.noreply.github.com>
2023-05-15 23:06:54 -0300

    Delete G7_Fioriti_Viegas_tp/Model directory

--- G7_Fioriti_Viegas_tp/Model/globos.cpp
+++
@@ -1,9 +0,0 @@
-#include "globos.h"
-
-globos::globos(string color, unsigned int stock,float precio, string tamanio, string marca) :cumpleanios(color, tamanio, precio, stock, marca) {
-
-}
-
-globos::~globos() {
-
-}
--- G7_Fioriti_Viegas_tp/Model/molde.h
+++
@@ -1,8 +0,0 @@
-
-
-#ifndef _MOLDE_H
-#define _MOLDE_H
-
-enum molde { torta, cupcakes, pan_dulce, ninguno };
-
-#endif //_MOLDE_H
