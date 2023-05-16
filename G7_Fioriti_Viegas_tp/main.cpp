3633f40e07b2e1b98294419b20352ec869e50614
Fedefioriti <ffioriti10@gmail.com>
2023-05-15 23:30:59 -0300

    cambio los include

--- G7_Fioriti_Viegas_tp/main.cpp
+++ G7_Fioriti_Viegas_tp/main.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 #include <forward_list>
 #include <string>
-#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/cotillon.h"
-#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/cliente.h"
-#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/disfraces.h"
-#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/globos.h"
-#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/golosinas.h"
-#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/manteles.h"
-#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/molde.h"
-#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/reposteria.h"
-#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/vajilla_descartable.h"
-#include "/Users/ffior/source/repos/G7_Fioriti_Viegas_tp/G7_Fioriti_Viegas_tp/Model/vela.h"
+#include "cotillon.h"
+#include "cliente.h"
+#include "disfraces.h"
+#include "globos.h"
+#include "golosinas.h"
+#include "manteles.h"
+#include "molde.h"
+#include "reposteria.h"
+#include "vajilla_descartable.h"
+#include "vela.h"
 
 using namespace std;
 
 int main() {
 
-    cotillon producto(10.0, 20, "disfrazate", mascara);
-    cotillon producto1(14.5, 15, "papelito", papel_picado);
-    cotillon producto2(5.0, 30, "laLuz", fluorescentes);
-    cotillon producto3(2.3, 13, "espumin", espuma);
+    cotillon producto(10.0, 20, "disfrazate", "mascara");
+    cotillon producto1(14.5, 15, "papelito", "papel_picado");
+    cotillon producto2(5.0, 30, "laLuz", "fluorescentes");
+    cotillon producto3(2.3, 13, "espumin", "espuma");
     disfraces producto4(30.0, 3, "disfrazate");
     globos producto5("amarillo", 20, 1.0, "chico", "bombucha");
     golosinas producto6("fellfort", 1.0, 50);
