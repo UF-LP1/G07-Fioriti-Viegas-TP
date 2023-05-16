58e88ba404b88b5e6aac4f659b655d763e3ef838
Fedefioriti <113440371+Fedefioriti@users.noreply.github.com>
2023-05-16 15:42:55 -0300

    Delete G7_Fioriti_Viegas_tp/Model directory

--- G7_Fioriti_Viegas_tp/Model/globos.cpp
+++
@@ -1,9 +0,0 @@
-#include "globos.h"
-
-globos::globos(string color, unsigned int stock,float precio, string tamanio, string marca,string nombre) :cumpleanios(color, tamanio, precio, stock, marca,nombre) {
-
-}
-
-globos::~globos() {
-
-}
--- G7_Fioriti_Viegas_tp/Model/librerias.h
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <iostream>
-#include <forward_list>
-#include <algorithm>
-#include <iterator>
-using namespace std;
