b5a5ae7bb73d0de7b959a98184b630796c3931bc
Fedefioriti <ffioriti10@gmail.com>
2023-05-16 14:45:34 -0300

    sera sera

--- G7_Fioriti_Viegas_tp/articulos_cotillon.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef _ARTICULOS_COTILLON_H
-#define _ARTICULOS_COTILLON_H
-
-enum articulos_cotillon {mascara, papel_picado, espuma, fluorescentes, luminosos, carteles_carton };
-
-#endif //_ARTICULOS_COTILLON_H
