80dc5ce122d176981ad32d1ebd449d9ac54e28f9
Fedefioriti <ffioriti10@gmail.com>
2023-05-16 16:01:49 -0300

    todos los archivos

---
+++ Model/matel.h
@@ -0,0 +1,11 @@
+/**
+ * Project Untitled
+ */
+
+
+#ifndef _MATEL_H
+#define _MATEL_H
+
+enum mantel { liso, clubes_futbol, jugadoes_selccion, superheroes, otros, a_imprimir };
+
+#endif //_MATEL_H
